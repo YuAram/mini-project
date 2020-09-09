ef17402e6e530e080d7810afacb8011dcaec37a4
shin-so-mi <shin-so-mi>
2020-09-09 17:30:53 +0900

    11111111

--- src/main/java/mini/project/Handler/MemberHandler.java
+++ src/main/java/mini/project/Handler/MemberHandler.java
@@ -1,10 +1,15 @@
 package mini.project.Handler;
 
+import java.util.List;
 import mini.project.domain.Member;
 import mini.project.util.Prompt;
 
 public class MemberHandler {
+  List<Member> memberList;
 
+  public MemberHandler(List<Member> list) {
+    this.memberList = list;
+  }
   public void test() {
     System.out.println("MemberHandler");
   }
@@ -14,6 +19,45 @@ public class MemberHandler {
 
     Member member = new Member();
     member.setName(Prompt.inputString("이름? "));
-    member.setGoalTypingNumber(Prompt.inputString("목표타수"));
+    member.setGoalTypingNumber(Prompt.inputInt("목표타수"));
+    
+    memberList.add(member);
   }
+  public void list() {
+    System.out.println("사용자 목록 조회");
+    
+    for (int i = 0; i < memberList.size(); i++) {
+      Member member = memberList.get(i);
+      System.out.printf("%d, %s, %d, %d, %d\n",
+          member.getNo(),
+          member.getName(),
+          member.getAverageTypingNumber(),
+          member.getGoalTypingNumber(),
+          member.getAccuracy());
+     
+      
+    }
+  }
+  
+  public void update() {
+    System.out.println("[회원 변경]");
+  
+    String name = Prompt.inputString(
+        String.format("이름(%s)? ", member.getName()));
+    String email = Prompt.inputString(
+        String.format("목표타수(%s)? ", member.getEmail()));
+
+
+    String response = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
+    if (!response.equalsIgnoreCase("y")) {
+      System.out.println("회원 변경을 취소하였습니다.");
+      return;
+      
+    }
+  }
+  
+  
+  
+  
+  
 }
