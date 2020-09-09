a5c2c72d23fec31a43d147b24c08df70e62f7cb0
shin-so-mi <shin-so-mi>
2020-09-09 15:02:43 +0900

    handler

--- src/main/java/mini/project/Handler/MemberHandler.java
+++ src/main/java/mini/project/Handler/MemberHandler.java
@@ -1,8 +1,19 @@
 package mini.project.Handler;
 
 public class MemberHandler {
+
   public void add() {
-    System.out.println("사용자 등록");
+    System.out.println("[사용자 등록]");
+
+    Member member = new Member();
+    member.setName(Prompt.inputString("이름? "));
+    member.setEmail(Prompt.inputString("목표타수"));
+    
+    
+
+    memberList.add(member);
   }
+
+
 
 }
