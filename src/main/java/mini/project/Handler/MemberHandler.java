5c1b40eefefb8904eef3febbd9b7c72746f8a8c8
shin-so-mi <shin-so-mi>
2020-09-09 15:09:08 +0900

    수정

--- src/main/java/mini/project/Handler/MemberHandler.java
+++ src/main/java/mini/project/Handler/MemberHandler.java
@@ -1,5 +1,8 @@
 package mini.project.Handler;
 
+import mini.project.domain.Member;
+import mini.project.util.Prompt;
+
 public class MemberHandler {
 
   public void add() {
@@ -7,7 +10,7 @@ public class MemberHandler {
 
     Member member = new Member();
     member.setName(Prompt.inputString("이름? "));
-    member.setEmail(Prompt.inputString("목표타수"));
+    member.setGoalTypingNumber(Prompt.inputString("목표타수"));
     
     
 
