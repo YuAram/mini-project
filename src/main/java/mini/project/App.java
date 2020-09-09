5d5209c375a75de7a4b8f3b3334f4ede9dfda398
YuAram <sukgi5319@naver.com>
2020-09-09 10:10:40 +0900

    테스트

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -5,7 +5,7 @@ package mini.project;
 
 public class App {
   public String getGreeting() {
-    return "Hello world..";
+    return "Hello world..아무거나";
   }
 
   public static void main(String[] args) {
